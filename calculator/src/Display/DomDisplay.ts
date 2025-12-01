4a14d14472968d64cca892ce737e8c3d018f2dac
Shiro-Nakano <nakano.shiro2001@gmail.com>
2025-12-01 17:56:58 +0900

    コメントアウトの変更・renderHistory()/clearHistory()の追加

--- calculator/src/Display/DomDisplay.ts
+++ calculator/src/Display/DomDisplay.ts
@@ -1,45 +1,76 @@
 /** 
  * クラス　DomDisplay               ディスプレイ周りのDom
+ * 【Private】
+ * -historyEl                      計算履歴のプロパティ
+ * -resultEl　　　　　　　　　　　　　　計算結果のプロパティ
  * 【Public】
- * +render(text: string)　　　　　　：途中式や入力時の数字の表示メソッド
- * +renderError(message: string)　：エラー表示時のメソッド
+ * +render(text: string)　　　　　     void　:途中式や入力時の数字の表示メソッド
+ * +renderError(message: string) 　　 void　:エラー表示時のメソッド
+ * +renderHistory(text: string)      void  :履歴エリアの表示メソッド
+ * +clearHistory()               　　 void  :履歴エリアの削除メソッド
  */
 
 // クラスのimport
-// ↓ディスプレイ周りのインターフェースを取得
 import type{ IDisplay } from "./IDisplay";
 
 export class DomDisplay implements IDisplay{
-    // HTMLDivタグ（ディスプレイ）のDom取得
-    private el: HTMLDivElement;
+    // 【Private】
+    private historyEl: HTMLDivElement;
+    private resultEl: HTMLDivElement;
+
 
     constructor (){
-    const element = document.getElementById("display") as HTMLDivElement;
-    // null時の例外処理（エラーケース）
-    if(!element){
+    // HTML(ディスプレイ計算履歴・結果）のDom取得
+    const historyEl = document.getElementById("history")as HTMLDivElement;
+    const resultEl = document.getElementById("result-display") as HTMLDivElement;
+
+    // 例外処理:null時エラーケース
+    if(!resultEl || !historyEl){
         throw new Error("ディスプレイに表示できるものがありません");
     }
-    this.el = element;
+    // 通常処理
+    else{
+        this.historyEl = historyEl;
+        this.resultEl = resultEl;
+    }
+    }
+
+    // 【Public】
+    /** 
+     * render                途中式や入力時の数字の表示メソッド
+     * @param {string}  text 画面に表示する文字列
+     * @return {void}　　　　　
+     */
+    public render(text: string): void {
+        this.resultEl.textContent = text;
     }
 
     /** 
-     * @param {string}  text 画面に表示する文字列
-     * @return {void}　　　　　画面への反映処理
-     */
-    public render(text: string): void {
-        this.el.textContent = text;
-    }
-    /** 
+     * renderError               エラー表示時のメソッド
      * @param {string} message 　エラー表示時ログ表示
-     * @return {void}　　　　　　　コンソール出力処理
+     * @return {void}　　　　　　　
      */
     public renderError(message: string): void {
-        this.el.textContent = message;
+        this.resultEl.textContent = message;
         // ⭐️コンソールエラーがいるか確認
         console.error("入力にエラーがあります");
         
     }
+
+    /** 
+     * renderHistory          履歴エリアの表示メソッド 
+     * @param {string}  text
+     * @return {void}　　　　　　　
+     */
     public renderHistory(text: string): void {
-        
+        this.historyEl.textContent = text;
     }
+
+    /** 
+     * renderHistory           履歴エリアの削除メソッド
+     * @return {void}　　　　　　　
+     */
+        public clearHistory(): void {
+            this.historyEl.textContent = "0";
+        }
 }
