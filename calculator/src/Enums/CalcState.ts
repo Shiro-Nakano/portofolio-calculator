bee03d0eefb85fe8bcc30eeb4feb4f3211c08265
Shiro-Nakano <nakano.shiro2001@gmail.com>
2025-12-01 19:32:27 +0900

    コメントアウトを追加

--- calculator/src/Enums/CalcState.ts
+++ calculator/src/Enums/CalcState.ts
@@ -1,5 +1,11 @@
 /** 
- * イーナム：電卓の状態
+ * イーナム　CalcState  　電卓の状態
+ * ready                初期状態（入力待ち）
+ * InputtingFirst　　　　１回目の入力（数字）
+ * OperatorEntered　　 　演算子入力
+ * InputtingSecond　　　 2回目の入力
+ * ResultShown　　　　　　計算結果表示
+ * Error　　　　　　　　　 エラー
  */
 
 export enum CalcState{
